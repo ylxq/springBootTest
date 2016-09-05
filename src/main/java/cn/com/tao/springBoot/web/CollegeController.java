4d74b5462835cf8b33d6990d50e029e9138d6342
taowenchen <254940751@qq.com>
2016-09-05 14:52:28 +0800

    html 添加

--- src/main/java/cn/com/tao/springBoot/web/CollegeController.java
+++ src/main/java/cn/com/tao/springBoot/web/CollegeController.java
@@ -3,22 +3,25 @@ package cn.com.tao.springBoot.web;
 import cn.com.tao.springBoot.domain.resporitory.CollegeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by taowenchen on 2016/9/5.
  */
+@RequestMapping
 @Controller
 public class CollegeController {
   private CollegeRepository collegeRepository;
 
-    @RequestMapping("/college")
-    @ResponseBody
-    public int getCollegeID(@PathVariable String name){
-        return collegeRepository.findByName(name).getId();
+
+    @RequestMapping(value = "/college", method = RequestMethod.GET)
+    public String getCollegeID(@RequestParam String name, Model model){
+        int id= collegeRepository.findByName(name).getId();
+        model.addAttribute("id",id);
+        return "/college/index";
     }
 
     @Autowired
